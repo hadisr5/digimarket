12a6179e738330b23482a87257e91737faa732bb
hadis.rastgar <h.rass1991@gmail.com>
2021-11-29 22:06:02 +0330

    Start Project :)

---
+++ DigiMarket.Persistence/Context/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using DigiMarket.Application.Interface.Context;
+using DigiMarket.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiMarket.Persistence.Context
+{
+    public class DataBaseContext : DbContext , IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+    }
+}
